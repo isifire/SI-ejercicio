27879e0bce7a6d4fa606e94998a0b2bbd813e7ae
isifire <isifire@outlook.com>
2025-02-08 18:37:56 +0100

    Conflictos resueltos en rebase

--- src/demo1/Bye.java
+++ src/demo1/Bye.java
@@ -7,11 +7,8 @@ public class Bye {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner teclado = new Scanner(System.in);
-<<<<<<< Upstream, based on master
 		Object o;
-=======
 		System.out.println("Prueba para el squash");
->>>>>>> f4cf1b0 Commit de conflicto con squash
 	}
 
 }
