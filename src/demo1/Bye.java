868710f163b99259b9a1c4e4d647eecb5f10326c
isifire <isifire@outlook.com>
2025-02-08 18:25:28 +0100

    Modificacion en master

---
+++ src/demo1/Bye.java
@@ -0,0 +1,12 @@
+package demo1;
+
+import java.util.Scanner;
+
+public class Bye {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner teclado = new Scanner(System.in);
+	}
+
+}
