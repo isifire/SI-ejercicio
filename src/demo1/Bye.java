19fcea96e5731e280fdf4bfb7430d7c5d88afa32
isifire <isifire@outlook.com>
2025-02-08 18:33:26 +0100

    Commit de conflicto con squash

--- src/demo1/Bye.java
+++ src/demo1/Bye.java
@@ -7,7 +7,11 @@ public class Bye {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner teclado = new Scanner(System.in);
+<<<<<<< Upstream, based on master
 		Object o;
+=======
+		System.out.println("Prueba para el squash");
+>>>>>>> f4cf1b0 Commit de conflicto con squash
 	}
 
 }
